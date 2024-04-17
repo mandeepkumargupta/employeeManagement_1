60b9e369a22689813ce384045c844522b138cdb4
Mandeep Kumar <mandeepgupta25792@gmail.com>
2024-04-17 23:48:15 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CreateItem from './components/CreateItem';
+
+
+function App() {
+  return (
+    <div>
+      <h2 className='custom-h2'>Employee management</h2>
+      <CreateItem />
+    </div>
+  );
+}
+
+export default App;
